af4b125f1f2b4d9fcd348f0b0727f3685d1a7a0e
AK46rocks <ak46rocks@gmail.com>
2023-11-01 00:29:38 +0530

    updated header links size in tablet view

--- frontend/src/metabase/nav/components/AppBar/DappLookerHeaderLinks.tsx
+++ frontend/src/metabase/nav/components/AppBar/DappLookerHeaderLinks.tsx
@@ -1,4 +1,29 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const LinkElement = styled.a`
+  color: #4c5773;
+  font-weight: 700;
+  min-width: fit-content;
+  position: relative;
+
+  @media screen and (max-width: 1024px) {
+    font-size: 12px;
+    line-height: 14px;
+    white-space: nowrap;
+  }
+`;
+
+const NewBadgeImg = styled.img`
+  position: absolute;
+  top: -11px;
+  right: -14px;
+
+  @media screen and (max-width: 1024px) {
+    width: 18px;
+    height: 18px;
+  }
+`;
 
 const DappLookerHeaderLinks = (): JSX.Element => {
   const DAPPLOOKER_MYCREATION_LINK =
@@ -6,7 +31,7 @@ const DappLookerHeaderLinks = (): JSX.Element => {
   const DAPPLOOKER_MYPROJECT_LINK = "https://dapplooker.com/user/dashboard";
   const ANALYZER_BROWSE_DATA_LINK = "https://analytics.dapplooker.com/browse/2";
 
-  const MYCREATION_NEW_BADGE_DATE = new Date("2023-11-12");
+  const MYCREATION_NEW_BADGE_EXPIRY_DATE = new Date("2023-11-12");
 
   const anchorTagStyle = {
     color: "#4C5773",
@@ -41,37 +66,37 @@ const DappLookerHeaderLinks = (): JSX.Element => {
         onMouseEnter={changeTextColorOnHover}
         onMouseLeave={resetTextColor}
         style={anchorTagStyle}>Discover</a> */}
-      <a
+      <LinkElement
         href={DAPPLOOKER_MYCREATION_LINK}
         onMouseEnter={changeTextColorOnHover}
         onMouseLeave={resetTextColor}
         style={anchorTagStyle}
       >
         My Dashboard
-      </a>
-      <a
+      </LinkElement>
+      <LinkElement
         href={DAPPLOOKER_MYPROJECT_LINK}
         onMouseEnter={changeTextColorOnHover}
         onMouseLeave={resetTextColor}
         style={anchorTagStyle}
       >
         My Creation
-        {isNewBadge(MYCREATION_NEW_BADGE_DATE) && (
-          <img
+        {isNewBadge(MYCREATION_NEW_BADGE_EXPIRY_DATE) && (
+          <NewBadgeImg
             style={newBadgeStyle}
             src={`app/assets/img/new-badge-header.svg`}
             alt="new badge"
           />
         )}
-      </a>
-      <a
+      </LinkElement>
+      <LinkElement
         href={ANALYZER_BROWSE_DATA_LINK}
         onMouseEnter={changeTextColorOnHover}
         onMouseLeave={resetTextColor}
         style={anchorTagStyle}
       >
         Browse Data
-      </a>
+      </LinkElement>
     </>
   );
 };
